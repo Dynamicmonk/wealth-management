e889cdf3b84e8f3bd7431f20c4f77e18aeea1604
Himanshu Kanojiya <himanshukanojiya825@gmail.com>
2022-09-29 12:20:56 +0530

    project setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { ReactLogo } from "./assets/images/imagesList";
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ReactLogo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.orgd"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <p>This is Sample App!</p>;
 }
 
 export default App;
---
+++ src/store/slices/sample/sample.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const sampleSlice = createSlice({
+  name: "authenticate",
+  initialState: {},
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export const {} = sampleSlice.actions;
+
+export default sampleSlice.reducer;
